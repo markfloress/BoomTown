9add687e868fb0c2ffb0d44288fb3c366e355462
Mark <marx.affiliate@gmail.com>
2017-10-30 09:46:58 -0700

    added base share card

--- client/src/components/cards/ShareCard.js
+++ client/src/components/cards/ShareCard.js
@@ -8,35 +8,33 @@ import moment from 'moment'
 
 class ShareCard extends Component {
   render() {
-    // const {available, borrower, created, description, imageurl, tags, itemowner, title} = this.props.data
     // console.log('with user', this.props.data)
-    // const createdDate = moment(created).fromNow()
+    const createdDate = moment().fromNow()
 
     return (
-      <p> lol </p>
-      //   <Card className="single-card">
-      //   <CardMedia 
-      //     overlay={ !available && <CardTitle subtitle="Unavailable" style={{ textTransform: "uppercase" }}/>}
-      //   >
-      //     <img src={imageurl} alt="Item Image" />
-      //   </CardMedia>
-      //   <Link to={`/profile/${itemowner.id}`}>
-      //     <CardHeader
-      //     title={itemowner.fullname}
-      //     subtitle={createdDate}
-      //     avatar={<Gravatar email={itemowner.email} style={{ borderRadius: "50%" }}/>}
-      //     />
-      //   </Link>
-      //   <CardTitle title={title} subtitle={tags.join(", ")} />
-      //   <CardText>
-      //     {description} 
-      //   </CardText>
-      //   <CardActions>
-      //     {!borrower && <RaisedButton label="Borrow" 
-      //     primary buttonStyle={{ backgroundColor: 'gray' }} 
-      //     labelStyle={{ textTransform: "uppercase" }}/>}
-      //   </CardActions>
-      // </Card>
+        <Card className="share-card" style={{width: "45%", marginLeft: "35%"}}>
+        {/* <CardMedia 
+          overlay={ !available && <CardTitle subtitle="Unavailable" style={{ textTransform: "uppercase" }}/>}
+        >
+          <img src="{imageurl}" alt="Item Image" />
+        </CardMedia> */}
+        <Link to="{`/profile/${itemowner.id}`}">
+          <CardHeader
+          title="{itemowner.fullname}"
+          subtitle="{createdDate}"
+
+          />
+        </Link>
+        <CardTitle/>
+        <CardText>
+          description
+        </CardText>
+        <CardActions>
+          {/* {!borrower && <RaisedButton label="Borrow" 
+          primary buttonStyle={{ backgroundColor: 'gray' }} 
+          labelStyle={{ textTransform: "uppercase" }}/>} */}
+        </CardActions>
+      </Card>
     )
   }
 }
